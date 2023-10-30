ce9a53b0e0bcc4ac6b20d688fffae76e33053fb8
Vinicio Chávez <viniciochz@gmail.com>
2023-10-30 16:27:46 -0600

    Added img smooth loading classes

---
+++ src/lib/hero-img.ts
@@ -0,0 +1,28 @@
+const LOADED_CLASS = "loaded-img"
+
+	const imgContainer = document.querySelector('.hero-image-container')
+	const isDiv = imgContainer instanceof HTMLDivElement
+
+	if (isDiv && imgContainer != null) {
+		const img = imgContainer.querySelector('img')
+		const isImage = img instanceof HTMLImageElement
+
+		if (isImage && img != null) {
+			if (img.complete) {
+				loaded(img)
+			}
+			else {
+				img.addEventListener('load', () => {
+					loaded(img)
+				})
+			}
+		}
+	}
+
+	
+	
+
+
+	function loaded(img: HTMLImageElement) {
+		img.classList.add(LOADED_CLASS)
+	}
